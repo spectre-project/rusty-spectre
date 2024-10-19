f24bbb7f7cf81fd1ca1cf7fe801df32170585a32
x100111010 <167847953+x100111010@users.noreply.github.com>
2024-10-19 02:50:05 +0200

    feat: next_block_template_feerate

--- mining/src/mempool/model/frontier/feerate_key.rs
+++ mining/src/mempool/model/frontier/feerate_key.rs
@@ -1,6 +1,4 @@
-use crate::block_template::selector::ALPHA;
-
-use super::tx::MempoolTransaction;
+use crate::{block_template::selector::ALPHA, mempool::model::tx::MempoolTransaction};
 use spectre_consensus_core::tx::Transaction;
 use std::sync::Arc;
 
@@ -22,6 +20,9 @@ impl PartialEq for FeerateTransactionKey {
 
 impl FeerateTransactionKey {
     pub fn new(fee: u64, mass: u64, tx: Arc<Transaction>) -> Self {
+        // NOTE: any change to the way this weight is calculated (such as scaling by some factor)
+        // requires a reversed update to total_weight in `Frontier::build_feerate_estimator`. This
+        // is because the math methods in FeeEstimator assume this specific weight function.
         Self { fee, mass, weight: (fee as f64 / mass as f64).powi(ALPHA), tx }
     }
 
@@ -49,13 +50,15 @@ impl PartialOrd for FeerateTransactionKey {
 
 impl Ord for FeerateTransactionKey {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        // Our first priority is the feerate
-        match self.feerate().total_cmp(&other.feerate()) {
+        // Our first priority is the feerate.
+        // The weight function is monotonic in feerate so we prefer using it
+        // since it is cached
+        match self.weight().total_cmp(&other.weight()) {
             core::cmp::Ordering::Equal => {}
             ord => return ord,
         }
 
-        // If feerates are equal, prefer the higher fee in absolute value
+        // If feerates (and thus weights) are equal, prefer the higher fee in absolute value
         match self.fee.cmp(&other.fee) {
             core::cmp::Ordering::Equal => {}
             ord => return ord,
@@ -74,6 +77,32 @@ impl Ord for FeerateTransactionKey {
 
 impl From<&MempoolTransaction> for FeerateTransactionKey {
     fn from(tx: &MempoolTransaction) -> Self {
-        Self::new(tx.mtx.calculated_fee.unwrap(), tx.mtx.tx.mass(), tx.mtx.tx.clone())
+        let mass = tx.mtx.tx.mass();
+        let fee = tx.mtx.calculated_fee.expect("fee is expected to be populated");
+        assert_ne!(mass, 0, "mass field is expected to be set when inserting to the mempool");
+        Self::new(fee, mass, tx.mtx.tx.clone())
+    }
+}
+
+#[cfg(test)]
+pub(crate) mod tests {
+    use super::*;
+    use spectre_consensus_core::{
+        subnets::SUBNETWORK_ID_NATIVE,
+        tx::{Transaction, TransactionInput, TransactionOutpoint},
+    };
+    use spectre_hashes::{HasherBase, TransactionID};
+    use std::sync::Arc;
+
+    fn generate_unique_tx(i: u64) -> Arc<Transaction> {
+        let mut hasher = TransactionID::new();
+        let prev = hasher.update(i.to_le_bytes()).clone().finalize();
+        let input = TransactionInput::new(TransactionOutpoint::new(prev, 0), vec![], 0, 0);
+        Arc::new(Transaction::new(0, vec![input], vec![], 0, SUBNETWORK_ID_NATIVE, 0, vec![]))
+    }
+
+    /// Test helper for generating a feerate key with a unique tx (per u64 id)
+    pub(crate) fn build_feerate_key(fee: u64, mass: u64, id: u64) -> FeerateTransactionKey {
+        FeerateTransactionKey::new(fee, mass, generate_unique_tx(id))
     }
 }
