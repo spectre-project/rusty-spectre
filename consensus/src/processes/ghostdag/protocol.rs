0be8768159e439086a2e2375ea5ac6b941c490de
x100111010 <167847953+x100111010@users.noreply.github.com>
2025-06-19 23:02:19 +0200

    fix crash when applying PP with DAG level 0 blocks referencing ORIGIN
    fix a crash that occurs at `let calculated_gd = self.ghostdag_manager.ghostdag(&parents)` when processing the first block in `proof[0]` (dag level 0). When parents contain [ORIGIN] so `ghostdag()` calls `find_selected_parent` which tries to read the ORIGIN's blue work from the ghostdag store via `get_blue_work(parent).unwrap()`, but `origin_ghostdag_data` was never inserted so it crashes with KeyNotFound. This also reverts 032fda16accfd12b57667df670afcd3ba126653e

--- consensus/src/processes/ghostdag/protocol.rs
+++ consensus/src/processes/ghostdag/protocol.rs
@@ -99,10 +99,7 @@ impl<T: GhostdagStoreReader, S: RelationsStoreReader, U: ReachabilityService, V:
     pub fn find_selected_parent(&self, parents: impl IntoIterator<Item = Hash>) -> Hash {
         parents
             .into_iter()
-            .map(|parent| SortableBlock {
-                hash: parent,
-                blue_work: self.ghostdag_store.get_blue_work(parent).unwrap_or(spectre_math::Uint192([0, 0, 0])),
-            })
+            .map(|parent| SortableBlock { hash: parent, blue_work: self.ghostdag_store.get_blue_work(parent).unwrap() })
             .max()
             .unwrap()
             .hash
